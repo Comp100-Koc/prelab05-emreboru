a0611e2856430f3fe4d27ecc6ae7921e2cc6cd7f
emreboru <eboru25@ku.edu.tr>
2026-04-01 20:15:41 +0300

    message

--- q1.py
+++ q1.py
@@ -1,5 +1,29 @@
 def longest_palindromic_substring(s):
-    """
-    Given a string find the longest palindromic substring
-    """
-    pass
+    max = 0
+    palindrome = ""
+    
+    for i in range(len(s)):
+        sol, sağ = i, i
+        
+        while sol >= 0 and sağ < len(s) and s[sol] == s[sağ]:
+            if (sağ - sol + 1)> max:
+                max = sağ - sol + 1
+                palindrome = s[sol:sağ + 1]
+                
+            sol -= 1
+            sağ += 1
+            
+        sol, sağ = i, i + 1 
+        
+        while sol >= 0 and sağ < len(s) and s[sol] == s[sağ]:
+            if (sağ-sol + 1 )> max:
+                max = sağ-sol + 1 
+                palindrome = s[sol:sağ + 1]
+                
+            sol -= 1
+            sağ += 1
+            
+    if max < 2:
+        return ""
+    
+    return palindrome
--- q3b.py
+++ q3b.py
@@ -1,5 +1,27 @@
+def binary_calculation(s):
+    binary_num = ""
+    while s > 0:
+        if s%2 == 1 :
+            binary_num += "1"
+        else:
+            binary_num += "0"
+        s = s//2
+    return binary_num[::-1]
+
+def making_number(n):
+    number = 0
+    length = len(n)
+    for i in range(length):
+        bit = int(n[length - 1 - i])
+        number += bit * (2 ** i)
+    return number
+
+
 def add_binary(a, b):
-    '''
-    Given two strings perform binary addition and return the result as a string
-    '''
-    pass
+    if a.startswith("0b"):
+        a = a[2:]
+    if b.startswith("0b"):
+        b = b[2:]
+    p = making_number(a) + making_number(b)
+    
+    return "0b" + binary_calculation(p)
